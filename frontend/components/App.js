41bde909a7d1da5bb2ca122cad020f49faa60b63
Michael Nobles <Mnobles33@gmail.com>
2024-12-07 13:36:11 -0700

    a good commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,6 +7,11 @@ export default class App extends React.Component {
   state = {
     todos: [],
     error: '',
+    todoNameInput: '',
+  }
+  onTodoNameInputChange = evt => {
+    const { value } = evt.target
+    this.setState({ ...this.state, todoNameInput: value })
   }
   fetchAllTodos = () => {
     axios.get(URL)
@@ -33,7 +38,7 @@ export default class App extends React.Component {
           }
         </div>
         <form id="todoForm">
-          <input type="text" placeholder="Type todo"></input>
+          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type todo"></input>
           <input type="submit"></input>
           <button>Clear Completed</button>
         </form>
